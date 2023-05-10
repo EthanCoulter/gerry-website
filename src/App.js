6ab2cede25a4324b694bf0499b323c51e903c836
Ethan Coulter <ethanscottcoulter@gmail.com>
2023-05-10 16:35:30 -0400

    Last update for the day

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from './components/Hero';
 import Book from './components/Book';
 import Header from './components/Header';
 import Testimonials from './components/Testimonials';
+import Bookapt from './components/Bookapt';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero/>
       <Book/>
       <Testimonials/>
+      <Bookapt/>
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import Typed from "react-typed";
 const Header = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen  bg-main-brand-col">
-      <h1 className="text-5xl font-bold pb-5 text-white tablet:text-6xl laptop:text-8xl">Gerald Walsh</h1>
+      <h1 className="text-[10vw] font-bold pb-5 text-white ">Gerald Walsh</h1>
       <h3 className=" items-center text-md align-center text-center mb-6 w-[vw] mx-2 text-white tablet:text-xl laptop:text-2xl">
         Supporting clients to make the next step towards a life that is{" "}
         <span className="uppercase font-bold">
@@ -17,7 +17,7 @@ const Header = () => {
         </span>
       </h3>
 
-      <button className="bg-dark-accent hover:bg-dark-shades text-white font-semibold hover:text-white py-2 px-4 border border-dark-shade rounded">
+      <button className="bg-transparent hover:bg-dark-accent text-white font-semibold hover:text-white py-2 px-4 border border-dark-shade rounded ease-in-out duration-200">
         Book Now
       </button>
     </div>
